e7babbd61353f0752be7df7562e173baf313cb18
matt <wootae.jung@beautyselection.co.kr>
2023-11-08 00:28:38 +0900

    init

---
+++ src/Timer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Timer() {
+  return (
+    <div>
+      <p>남은 시간을 표시하세요</p>
+      <button>시작</button>
+      <button>리셋</button>
+    </div>
+  );
+}
+
+export default Timer;
